eed706dc8ba8e07d50de71934ead7c68257b94bf
Ross <rbunn1997@gmail.com>
2019-07-01 09:56:56 +0100

    updated the about us page

--- page-about-us.php
+++ page-about-us.php
@@ -3,22 +3,18 @@
     <div class="container" role="main">
 
 	    <div class="row">
-	    	<div class="col-md-8">
-NEW CUSTOM ABOUT PAGE
+	    	<div class="col-md-12">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	
 				    <div class="page-header">	
 				    	<h1><?php the_title(); ?></h1>
 				    </div>
 
-
-					<?php the_content(); ?>
+					<?php the_content();?>
 
 				<?php endwhile; endif; ?>
 
 	    	</div>
-
-	    	<?php get_sidebar(); ?>
 
 	    </div>
 
